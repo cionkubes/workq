4ebcd8bd243af4140b44fb932f1cf7872c773f05
Harald Floor Wilhelmsen <harald.floor.wilhelmsen@trondheim.kommune.no>
2018-02-02 13:59:12 +0100

    Removed duplicate code. Fixed typo in build script. Fixed deployment

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-docker build -f Dockerfile-worker . --tag cion/worker:${1:-lastest}
+docker build -f Dockerfile-worker . --tag cion/worker:${1:-latest}
 docker build -f Dockerfile-orchestrator . --tag cion/orchestrator:${1:-latest}
