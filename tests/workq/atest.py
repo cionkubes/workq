970272a1b5502bf47a25ed6a43e3256bd3b81720
Erlend <erlentob@stud.ntnu.no>
2018-02-08 21:42:50 +0100

    9

--- tests/workq/atest.py
+++ tests/workq/atest.py
@@ -3,7 +3,7 @@ import asyncio
 
 
 @pytest.mark.asyncio
-async def test_many_separate_files(objects):
+async def test_many_separate_files(streampair_generator, objects):
     await asyncio.sleep(3)
 
     for object in objects:
