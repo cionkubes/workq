8b956e6170469489e4c76663eb6dd2e9b0481c81
Erlend <erlentob@stud.ntnu.no>
2018-02-08 21:34:35 +0100

    6

--- tests/workq/stream.py
+++ tests/workq/stream.py
@@ -9,7 +9,8 @@ from workq.apickle import dump, load
 async def test_readline(streampair, event_loop):
     r, w = streampair
 
-    lines = [b'nHuJONnMFE\n', b'W66tOBAfrw943gh\n', b'SyDMv143gaGE#%@274577&$TTHU\n']
+    lines = [b'nHuJONnMFE\n', b'W66tOBAfrw943gh\n',
+             b'SyDMv143gaGE#%@274577&$TTHU\n']
 
     asyncio.ensure_future(w.write(b''.join(lines)), loop=event_loop)
 
@@ -21,7 +22,8 @@ async def test_readline(streampair, event_loop):
 async def test_read_exactly(streampair, event_loop):
     r, w = streampair
 
-    strings = [b'nHuJONnMFE', b'W66tOBAfrw943gh', b'SyDMv143gaGE#%@274577&$TTHU']
+    strings = [b'nHuJONnMFE', b'W66tOBAfrw943gh',
+               b'SyDMv143gaGE#%@274577&$TTHU']
 
     asyncio.ensure_future(w.write(b''.join(strings)), loop=event_loop)
 
@@ -36,6 +38,8 @@ async def test_many_separate_streams(streampair_generator, objects):
         await dump(obj, w)
         assert obj == await load(r)
 
+    print("Done")
+
 
 @pytest.mark.asyncio
 async def test_many_same_stream(streampair, objects):
@@ -44,3 +48,5 @@ async def test_many_same_stream(streampair, objects):
     for obj in objects:
         await dump(obj, w)
         assert obj == await load(r)
+
+    print("Done 2")
