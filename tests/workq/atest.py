1787244bfb5edbec914dd0a2af7e8df8f93e95df
Erlend <erlentob@stud.ntnu.no>
2018-02-08 22:30:25 +0100

    19

--- tests/workq/atest.py
+++ tests/workq/atest.py
@@ -4,12 +4,21 @@ import asyncio
 from workq.apickle import dump, load
 
 
+# @pytest.mark.asyncio
+# async def test_many_separate_streams(streampair_generator, objects):
+#     r, w = next(streampair_generator)
+#     o = objects[0]
+#     for streampair, obj in zip(streampair_generator, objects):
+#         r, w = streampair
+
+#         await dump(o, w)
+#         assert o == await load(r)
+
+
 @pytest.mark.asyncio
 async def test_many_separate_streams(streampair_generator, objects):
     r, w = next(streampair_generator)
-    o = objects[0]
     for streampair, obj in zip(streampair_generator, objects):
-        r, w = streampair
 
-        await dump(o, w)
-        assert o == await load(r)
+        await dump(obj, w)
+        assert obj == await load(r)
