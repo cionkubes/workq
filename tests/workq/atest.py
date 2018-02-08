34448a0e255f8d23c24d9d16afd13653e18a7e0a
Erlend <erlentob@stud.ntnu.no>
2018-02-08 22:12:39 +0100

    15

--- tests/workq/atest.py
+++ tests/workq/atest.py
@@ -7,6 +7,6 @@ from workq.apickle import dump, load
 @pytest.mark.asyncio
 async def test_many_separate_streams(streampair_generator, objects):
     r, w = next(streampair_generator)
-    obj = next(objects)
+    obj = objects[0]
     await dump(obj, w)
     assert obj == await load(r)
