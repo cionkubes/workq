a5d10e23ee09640c0aca931ffc41ab5aa9e7b065
Erlend <erlentob@stud.ntnu.no>
2018-02-11 16:15:59 +0100

    26

--- tests/workq/apickle.py
+++ tests/workq/apickle.py
@@ -7,7 +7,9 @@ from workq.apickle import load, dump
 
 @pytest.mark.asyncio
 async def test_many_separate_files(event_loop, filepair_generator, objects):
+    print("1")
     for obj, filepair in zip(objects, filepair_generator):
+        print("2")
         r, w = filepair
         write_task = asyncio.ensure_future(dump(obj, w), loop=event_loop)
         read_task = asyncio.ensure_future(load(r), loop=event_loop)
@@ -19,7 +21,9 @@ async def test_many_separate_files(event_loop, filepair_generator, objects):
 async def test_many_same_files(event_loop, filepair, objects):
     r, w = filepair
 
+    print("3")
     for obj in objects:
+        print("4")
         write_task = asyncio.ensure_future(dump(obj, w), loop=event_loop)
         read_task = asyncio.ensure_future(load(r), loop=event_loop)
         await write_task
